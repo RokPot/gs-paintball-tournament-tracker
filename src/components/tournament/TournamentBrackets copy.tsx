a82b96d61dc9342348b0a9f082d073b828fe1103
Rok Potočnik <rok.potocnik@povio.com>
2023-11-29 11:48:18 +0100

    wip: brackets, working-ish

---
+++ src/components/tournament/TournamentBrackets copy.tsx
@@ -0,0 +1,221 @@
+import { Theme, css, styled } from '@mui/material';
+import BracketsGameRow from 'components/brackets/BracketsGameRow';
+import FlexContainer from 'components/shared/FlexContainer';
+import League from 'types/League';
+
+const teamRowHeight = 40;
+const firstPadding = 40 + Math.ceil(40 / 4);
+const firstLevelHeight = 2 * teamRowHeight + 20;
+interface VerticalArrowProps {
+  height: number;
+}
+
+const StyledArrowVertical = styled('div')(
+  (props: VerticalArrowProps & { theme?: Theme }) => css`
+    border: 2px solid ${props.theme?.palette.primary.light};
+    width: 60px;
+    margin-top: 0px;
+    height: ${props.height}px;
+    border-left: none;
+    border-radius: 0px 5px 5px 0px;
+    border-collapse: collapse;
+  `,
+);
+const StyledArrowHorizontal = styled('div')(
+  (props) => css`
+    border: 1.5px solid ${props.theme.palette.primary.light};
+
+    width: 60px;
+
+    height: 1px;
+    border-left: none;
+    border-radius: 0px 5px 5px 0px;
+  `,
+);
+interface IProps {
+  activeLeague: League;
+}
+
+const TournamentBrackets = ({ activeLeague }: IProps) => {
+  const selectedTournament = activeLeague?.activeTournament;
+
+  if (!selectedTournament || !activeLeague) {
+    return null;
+  }
+  const level1 = 2;
+
+  const firstLevelHeightContainer = 2 ** 0 * firstLevelHeight + 1 * 2;
+
+  const secondLevelHeightContainer = 2 ** 1 * firstLevelHeight + 1 * 2;
+
+  const thirdLevelHeightContainer = 2 ** 2 * firstLevelHeight + 1 * 2;
+
+  const fourthLevelHeightContainer = 2 ** 3 * firstLevelHeight + 1 * 2;
+
+  const numberOfGames = 8;
+  let numOfLayers = 0;
+  while (numberOfGames > 2 ** numOfLayers) {
+    numOfLayers += 1;
+  }
+  numOfLayers += 1;
+  // 2 ^( n - 1) + 1
+  // ali 2 ^ n - 1
+
+  const test = [
+    {
+      layer: 0,
+    },
+    {
+      layer: 0,
+    },
+    {
+      layer: 0,
+    },
+    {
+      layer: 0,
+    },
+    {
+      layer: 0,
+    },
+    {
+      layer: 0,
+    },
+    {
+      layer: 0,
+    },
+    {
+      layer: 0,
+    },
+    {
+      layer: 1,
+    },
+    {
+      layer: 1,
+    },
+    {
+      layer: 1,
+    },
+    {
+      layer: 1,
+    },
+    {
+      layer: 2,
+    },
+    {
+      layer: 2,
+    },
+    {
+      layer: 3,
+    },
+  ];
+  return (
+    <FlexContainer
+      flexDirection="row"
+      alignItems="flex-start"
+      padding="20px 0px 0px 0px"
+    >
+      <FlexContainer
+        flexDirection="column"
+        margin={Math.floor(teamRowHeight / 2)}
+      >
+        <BracketsGameRow />
+        <BracketsGameRow />
+        <BracketsGameRow />
+        <BracketsGameRow />
+        <BracketsGameRow />
+        <BracketsGameRow />
+        <BracketsGameRow />
+        <BracketsGameRow />
+      </FlexContainer>
+      <FlexContainer
+        flexDirection="column"
+        style={{ marginLeft: '-5px', marginRight: '-5px' }}
+        margin={Math.floor(teamRowHeight / 2)}
+      >
+        <FlexContainer style={{ paddingTop: `${teamRowHeight - 1}px` }}>
+          <StyledArrowVertical height={firstLevelHeightContainer} />
+          <StyledArrowHorizontal />
+        </FlexContainer>
+        <FlexContainer
+          style={{ paddingTop: `${level1 * teamRowHeight - 2}px` }}
+        >
+          <StyledArrowVertical height={firstLevelHeightContainer} />
+          <StyledArrowHorizontal />
+        </FlexContainer>
+        <FlexContainer
+          style={{ paddingTop: `${level1 * teamRowHeight - 2}px` }}
+        >
+          <StyledArrowVertical height={firstLevelHeightContainer} />
+          <StyledArrowHorizontal />
+        </FlexContainer>
+        <FlexContainer
+          style={{ paddingTop: `${level1 * teamRowHeight - 2}px` }}
+        >
+          <StyledArrowVertical height={firstLevelHeightContainer} />
+          <StyledArrowHorizontal />
+        </FlexContainer>
+      </FlexContainer>
+      <FlexContainer
+        flexDirection="column"
+        margin={teamRowHeight * 3}
+        padding={`${firstPadding}px 0px 0px 0px`}
+      >
+        <BracketsGameRow />
+        <BracketsGameRow />
+        <BracketsGameRow />
+        <BracketsGameRow />
+      </FlexContainer>
+      <FlexContainer
+        flexDirection="column"
+        style={{ marginLeft: '-5px', marginRight: '-5px' }}
+        margin={teamRowHeight * 3}
+      >
+        <FlexContainer
+          style={{ paddingTop: `${teamRowHeight - 1 + firstPadding}px` }}
+        >
+          <StyledArrowVertical height={secondLevelHeightContainer} />
+          <StyledArrowHorizontal />
+        </FlexContainer>
+        <FlexContainer
+          style={{
+            paddingTop: `${teamRowHeight * 2 - 2}px`,
+          }}
+        >
+          <StyledArrowVertical height={secondLevelHeightContainer} />
+          <StyledArrowHorizontal />
+        </FlexContainer>
+      </FlexContainer>
+      <FlexContainer
+        flexDirection="column"
+        margin={teamRowHeight * 8}
+        padding={`${3 * firstPadding}px 0px 0px 0px`}
+      >
+        <BracketsGameRow />
+        <BracketsGameRow />
+      </FlexContainer>
+      <FlexContainer
+        flexDirection="column"
+        style={{ marginLeft: '-5px', marginRight: '-5px' }}
+        margin={teamRowHeight * 8}
+      >
+        <FlexContainer
+          style={{
+            paddingTop: `${1 * teamRowHeight - 1 + 3 * firstPadding}px`,
+          }}
+        >
+          <StyledArrowVertical height={thirdLevelHeightContainer} />
+          <StyledArrowHorizontal />
+        </FlexContainer>
+      </FlexContainer>
+      <FlexContainer
+        flexDirection="column"
+        margin={teamRowHeight * 18}
+        padding={`${7 * firstPadding}px 0px 0px 0px`}
+      >
+        <BracketsGameRow />
+      </FlexContainer>
+    </FlexContainer>
+  );
+};
+
+export default TournamentBrackets;
--- src/components/tournament/TournamentBrackets.tsx
+++ src/components/tournament/TournamentBrackets.tsx
@@ -6,7 +6,6 @@ import League from 'types/League';
 const teamRowHeight = 40;
 const firstPadding = 40 + Math.ceil(40 / 4);
 const firstLevelHeight = 2 * teamRowHeight + 20;
-const secondLevelHeight = 100;
 interface VerticalArrowProps {
   height: number;
 }
@@ -14,7 +13,7 @@ interface VerticalArrowProps {
 const StyledArrowVertical = styled('div')(
   (props: VerticalArrowProps & { theme?: Theme }) => css`
     border: 2px solid ${props.theme?.palette.primary.light};
-    width: 60px;
+    width: 70px;
     margin-top: 0px;
     height: ${props.height}px;
     border-left: none;
@@ -24,11 +23,9 @@ const StyledArrowVertical = styled('div')(
 );
 const StyledArrowHorizontal = styled('div')(
   (props) => css`
-    border: 1px solid ${props.theme.palette.primary.light};
-
-    width: 60px;
-
-    height: 1px;
+    border: 1.5px solid ${props.theme.palette.primary.light};
+    width: 70px;
+    height: 0.5px;
     border-left: none;
     border-radius: 0px 5px 5px 0px;
   `,
@@ -43,130 +40,219 @@ const TournamentBrackets = ({ activeLeague }: IProps) => {
   if (!selectedTournament || !activeLeague) {
     return null;
   }
-  const level1 = 2;
-
-  const firstLevelHeightContainer = firstLevelHeight + 1 * 2;
-
-  const secondLevelHeightContainer = 2 * firstLevelHeight + 1 * 2;
-
-  const thirdLevelHeightContainer = 4 * firstLevelHeight + 1 * 2;
-
-  const fourthLevelHeightContainer = 8 * firstLevelHeight + 1 * 2;
-
-  const numberOfTeams = 8;
+  const numberOfGames = 16;
   let numOfLayers = 0;
-  while (numberOfTeams > 2 ** numOfLayers) {
+  while (numberOfGames > 2 ** numOfLayers) {
     numOfLayers += 1;
   }
+  numOfLayers += 1;
   // 2 ^( n - 1) + 1
   // ali 2 ^ n - 1
+
+  const test2 = [
+    {
+      layer: 0,
+    },
+    {
+      layer: 0,
+    },
+    {
+      layer: 0,
+    },
+    {
+      layer: 0,
+    },
+    {
+      layer: 0,
+    },
+    {
+      layer: 0,
+    },
+    {
+      layer: 0,
+    },
+    {
+      layer: 0,
+    },
+
+    {
+      layer: 1,
+    },
+    {
+      layer: 1,
+    },
+    {
+      layer: 1,
+    },
+    {
+      layer: 1,
+    },
+    {
+      layer: 2,
+    },
+    {
+      layer: 2,
+    },
+    {
+      layer: 3,
+    },
+  ];
+
+  const test = [
+    {
+      layer: 0,
+    },
+    {
+      layer: 0,
+    },
+    {
+      layer: 0,
+    },
+    {
+      layer: 0,
+    },
+    {
+      layer: 0,
+    },
+    {
+      layer: 0,
+    },
+    {
+      layer: 0,
+    },
+    {
+      layer: 0,
+    },
+    {
+      layer: 0,
+    },
+    {
+      layer: 0,
+    },
+    {
+      layer: 0,
+    },
+    {
+      layer: 0,
+    },
+    {
+      layer: 0,
+    },
+    {
+      layer: 0,
+    },
+    {
+      layer: 0,
+    },
+    {
+      layer: 0,
+    },
+    {
+      layer: 1,
+    },
+    {
+      layer: 1,
+    },
+    {
+      layer: 1,
+    },
+    {
+      layer: 1,
+    },
+    {
+      layer: 1,
+    },
+    {
+      layer: 1,
+    },
+    {
+      layer: 1,
+    },
+    {
+      layer: 1,
+    },
+    {
+      layer: 2,
+    },
+    {
+      layer: 2,
+    },
+    {
+      layer: 2,
+    },
+    {
+      layer: 2,
+    },
+    {
+      layer: 3,
+    },
+    {
+      layer: 3,
+    },
+    {
+      layer: 4,
+    },
+  ];
   return (
     <FlexContainer
       flexDirection="row"
       alignItems="flex-start"
       padding="20px 0px 0px 0px"
     >
-      <FlexContainer
-        flexDirection="column"
-        margin={Math.floor(teamRowHeight / 2)}
-      >
-        <BracketsGameRow />
-        <BracketsGameRow />
-        <BracketsGameRow />
-        <BracketsGameRow />
-        <BracketsGameRow />
-        <BracketsGameRow />
-        <BracketsGameRow />
-        <BracketsGameRow />
-      </FlexContainer>
-      <FlexContainer
-        flexDirection="column"
-        style={{ marginLeft: '-5px', marginRight: '-5px' }}
-        margin={Math.floor(teamRowHeight / 2)}
-      >
-        <FlexContainer style={{ paddingTop: `${teamRowHeight - 1}px` }}>
-          <StyledArrowVertical height={firstLevelHeightContainer} />
-          <StyledArrowHorizontal />
-        </FlexContainer>
-        <FlexContainer
-          style={{ paddingTop: `${level1 * teamRowHeight - 2}px` }}
-        >
-          <StyledArrowVertical height={firstLevelHeightContainer} />
-          <StyledArrowHorizontal />
-        </FlexContainer>
-        <FlexContainer
-          style={{ paddingTop: `${level1 * teamRowHeight - 2}px` }}
-        >
-          <StyledArrowVertical height={firstLevelHeightContainer} />
-          <StyledArrowHorizontal />
-        </FlexContainer>
-        <FlexContainer
-          style={{ paddingTop: `${level1 * teamRowHeight - 2}px` }}
-        >
-          <StyledArrowVertical height={firstLevelHeightContainer} />
-          <StyledArrowHorizontal />
-        </FlexContainer>
-      </FlexContainer>
+      {[...Array(numOfLayers)].map((val, index) => {
+        const layer = index;
+        const layerConstants = [0.5, 3, 8, 18];
+        const getContainerMargin = (depth: number) => {
+          return layerConstants[depth] * teamRowHeight;
+        };
+        const getArrowMargin = (depth: number) => {
+          return layerConstants[depth] * teamRowHeight + 2 * teamRowHeight - 2;
+        };
+        const getContainerPadding = (depth: number) => {
+          return depth === 0 ? 0 : (2 ** depth - 1) * firstPadding;
+        };
+        const getArrowPadding = (depth: number) => {
+          if (depth === 0) {
+            return teamRowHeight - 1;
+          }
+          return (2 ** depth - 1) * firstPadding + teamRowHeight - 1;
+        };
 
-      <FlexContainer
-        flexDirection="column"
-        margin={teamRowHeight * 3}
-        padding={`${firstPadding}px 0px 0px 0px`}
-      >
-        <BracketsGameRow />
-        <BracketsGameRow />
-        <BracketsGameRow />
-        <BracketsGameRow />
-      </FlexContainer>
-      <FlexContainer
-        flexDirection="column"
-        style={{ marginLeft: '-5px', marginRight: '-5px' }}
-        margin={teamRowHeight * 3}
-      >
-        <FlexContainer
-          style={{ paddingTop: `${teamRowHeight - 1 + firstPadding}px` }}
-        >
-          <StyledArrowVertical height={secondLevelHeightContainer} />
-          <StyledArrowHorizontal />
-        </FlexContainer>
-        <FlexContainer
-          style={{
-            paddingTop: `${teamRowHeight * 2 - 2}px`,
-          }}
-        >
-          <StyledArrowVertical height={secondLevelHeightContainer} />
-          <StyledArrowHorizontal />
-        </FlexContainer>
-      </FlexContainer>
-      <FlexContainer
-        flexDirection="column"
-        margin={teamRowHeight * 8}
-        padding={`${3 * firstPadding}px 0px 0px 0px`}
-      >
-        <BracketsGameRow />
-        <BracketsGameRow />
-      </FlexContainer>
-      <FlexContainer
-        flexDirection="column"
-        style={{ marginLeft: '-5px', marginRight: '-5px' }}
-        margin={teamRowHeight * 8}
-      >
-        <FlexContainer
-          style={{
-            paddingTop: `${1 * teamRowHeight - 1 + 3 * firstPadding}px`,
-          }}
-        >
-          <StyledArrowVertical height={thirdLevelHeightContainer} />
-          <StyledArrowHorizontal />
-        </FlexContainer>
-      </FlexContainer>
-      <FlexContainer
-        flexDirection="column"
-        margin={teamRowHeight * 18}
-        padding={`${7 * firstPadding}px 0px 0px 0px`}
-      >
-        <BracketsGameRow />
-      </FlexContainer>
+        const getArrowsHeight = (depth: number) => {
+          return 2 ** depth * firstLevelHeight + 1 * 2;
+        };
+
+        return (
+          <>
+            <FlexContainer
+              flexDirection="column"
+              margin={getContainerMargin(layer)}
+              padding={`${getContainerPadding(layer)}px 0px 0px 0px`}
+            >
+              {test
+                .filter((obj) => obj.layer === layer)
+                .map((_) => (
+                  <BracketsGameRow />
+                ))}
+            </FlexContainer>
+            <FlexContainer
+              flexDirection="column"
+              style={{ marginLeft: '-7px', marginRight: '-7px' }}
+              margin={getArrowMargin(layer)}
+              padding={`${getArrowPadding(layer)}px 0px 0px 0px`}
+            >
+              {test
+                .filter((obj) => obj.layer === layer + 1)
+                .map(() => (
+                  <FlexContainer>
+                    <StyledArrowVertical height={getArrowsHeight(layer)} />
+                    <StyledArrowHorizontal />
+                  </FlexContainer>
+                ))}
+            </FlexContainer>
+          </>
+        );
+      })}
     </FlexContainer>
   );
 };
