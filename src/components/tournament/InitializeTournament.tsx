6e068f1017ec7342c96cfaf550edc29c7d410723
Rok Potočnik <rok.potocnik@povio.com>
2023-12-10 18:44:00 +0100

    wip: tournament type set up

--- src/components/tournament/InitializeTournament.tsx
+++ src/components/tournament/InitializeTournament.tsx
@@ -1,5 +1,6 @@
 import {
   Button,
+  FormControl,
   InputLabel,
   MenuItem,
   Select,
@@ -11,6 +12,7 @@ import { useCallback, useState } from 'react';
 import Tournament from 'types/Tournament';
 import { TournamentGroup } from 'types/TournamentGroup';
 import { TournamentSettings } from 'types/TournamentSettings';
+import { TournamentType, TournamentTypeLabels } from 'types/TournamentType';
 import { v4 } from 'uuid';
 
 interface IProps {
@@ -59,21 +61,40 @@ const InitializeTournament = ({ tournament }: IProps) => {
       width="100%"
     >
       <Typography variant="h1">Initialize {tournament.name}</Typography>
-      <InputLabel>Number of groups</InputLabel>
+      <FlexContainer flexDirection="row" width="100%">
+        <FormControl fullWidth>
+          <InputLabel>Number of groups</InputLabel>
 
-      <Select
-        value={formik?.values?.numberOfGroups}
-        label="Team size"
-        onChange={(e) =>
-          formik.setFieldValue('numberOfGroups', Number(e.target.value))
-        }
-      >
-        {[1, 2, 3, 4, 5, 6]?.map((teamSize, index) => (
-          <MenuItem key={index} value={teamSize}>
-            {teamSize}-man
-          </MenuItem>
-        ))}
-      </Select>
+          <Select
+            value={formik?.values?.numberOfGroups}
+            label="Number of groups"
+            onChange={(e) =>
+              formik.setFieldValue('numberOfGroups', Number(e.target.value))
+            }
+          >
+            {[1, 2, 3, 4, 5, 6]?.map((numberOfGroups, index) => (
+              <MenuItem key={index} value={numberOfGroups}>
+                {numberOfGroups}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <FormControl fullWidth>
+          <InputLabel>Type</InputLabel>
+          <Select
+            value={tournament?.settings.type}
+            label="Type"
+            onChange={(e) => {}}
+          >
+            {Object.values(TournamentType)?.map((tournamentKey, index) => (
+              <MenuItem key={index} value={tournamentKey}>
+                {TournamentTypeLabels[tournamentKey]}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </FlexContainer>
+
       <FlexContainer flexDirection="column" width="100%">
         <FlexContainer
           flexDirection="row"
