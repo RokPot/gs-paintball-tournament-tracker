aa9ec312c5dc53120f83e274d391bcb95d995f1f
Rok Potočnik <rok.potocnik@povio.com>
2023-12-14 11:34:52 +0100

    initialize tournament 2

--- src/components/tournament/InitializeTournament.tsx
+++ src/components/tournament/InitializeTournament.tsx
@@ -1,10 +1,14 @@
 import {
   Button,
+  Card,
   FormControl,
   InputLabel,
   MenuItem,
   Select,
   Typography,
+  alpha,
+  styled,
+  useTheme,
 } from '@mui/material';
 import FlexContainer from 'components/shared/FlexContainer';
 import { useFormik } from 'formik';
@@ -17,10 +21,12 @@ import { v4 } from 'uuid';
 
 interface IProps {
   tournament: Tournament;
+  className?: string;
 }
 
-const InitializeTournament = ({ tournament }: IProps) => {
+const InitializeTournament: React.FC<IProps> = ({ tournament, className }) => {
   const [groups, setGroups] = useState(tournament.groups);
+  const theme = useTheme();
   const shuffleArray = <T,>(array: T[]): T[] => {
     const newArray = [...array];
     for (let i = newArray.length - 1; i > 0; i -= 1) {
@@ -59,6 +65,7 @@ const InitializeTournament = ({ tournament }: IProps) => {
       alignItems="flex-start"
       margin={16}
       width="100%"
+      className={className}
     >
       <Typography variant="h1">Initialize {tournament.name}</Typography>
       <FlexContainer flexDirection="row" width="100%">
@@ -68,9 +75,11 @@ const InitializeTournament = ({ tournament }: IProps) => {
           <Select
             value={formik?.values?.numberOfGroups}
             label="Number of groups"
-            onChange={(e) =>
-              formik.setFieldValue('numberOfGroups', Number(e.target.value))
-            }
+            onChange={(e) => {
+              formik.setFieldValue('numberOfGroups', Number(e.target.value));
+              tournament.settings.numberOfGroups = Number(e.target.value);
+              shuffleTeams();
+            }}
           >
             {[1, 2, 3, 4, 5, 6]?.map((numberOfGroups, index) => (
               <MenuItem key={index} value={numberOfGroups}>
@@ -106,12 +115,29 @@ const InitializeTournament = ({ tournament }: IProps) => {
         </FlexContainer>
         <FlexContainer flexDirection="row">
           {groups.map((group) => (
-            <FlexContainer flexDirection="column">
-              Group {group.groupIndex}
-              {group?.teams.map((team) => (
-                <Typography>{team.teamName}</Typography>
-              ))}
-            </FlexContainer>
+            <Card className="custom-card counter-card">
+              <FlexContainer flexDirection="column">
+                <Typography
+                  variant="h3Medium"
+                  borderBottom={`1px solid ${theme.palette.divider}`}
+                  width="100%"
+                >
+                  Group {group.groupIndex}
+                </Typography>
+
+                <FlexContainer
+                  highlightRowOnHover
+                  flexDirection="column"
+                  width="100%"
+                >
+                  {group?.teams.map((team) => (
+                    <div style={{ padding: '8px', width: '100%' }}>
+                      <Typography>{team.teamName}</Typography>
+                    </div>
+                  ))}
+                </FlexContainer>
+              </FlexContainer>
+            </Card>
           ))}
         </FlexContainer>
       </FlexContainer>
@@ -119,4 +145,15 @@ const InitializeTournament = ({ tournament }: IProps) => {
   );
 };
 
-export default InitializeTournament;
+export default styled(InitializeTournament)(
+  (props) => `
+    .custom-card {
+      box-shadow: ${alpha(props.theme.palette.primary.main, 0.5)} 0px 5px 15px;
+      border: solid 1px ${alpha(props.theme.palette.primary.main, 0.2)};
+      margin: 8px;
+      padding: 16px;
+      min-width: 200px;
+      min-height: 300px;
+    }
+  `,
+);
